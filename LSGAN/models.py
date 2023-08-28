f2ed7dbddeadfa2f99bc477d56f37255ff988b0b
Osamah-ElRadaideh <142032834+Osamah-ElRadaideh@users.noreply.github.com>
2023-08-28 20:24:33 +0200

    updated models, fixed channel sizes

--- LSGAN/models.py
+++ LSGAN/models.py
@@ -13,20 +13,20 @@ class up_block(nn.Module):
     #up_sampling block of the generator
     def __init__(self, in_channels, out_channels, kernel_size=1, stride=1,act='relu'):
         super().__init__()
-        assert act.lower() in ['relu', 'sigmoid'], f'act must be either relu or sigmoid, got {act} instead'
+        assert act.lower() in ['relu', 'tanh'], f'act must be either relu or tanh, got {act} instead'
         self.act = act
         self.conv = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride)
         self.bn = nn.BatchNorm2d(out_channels)
-        self.relu = nn.LeakyReLU(inplace=True, negative_slope=0.1)
-        self.sigmoid = nn.Sigmoid()
+        self.relu = nn.ReLU(inplace=True)
+        self.tanh = nn.Tanh()
     def forward(self, x):
         x = self.conv(x)
         x = self.bn(x)
         if self.act =='relu':
             return(self.relu(x))
         else:
-         
-            return(self.sigmoid(x))
+            # return x 
+            return(self.tanh(x))
 
 class down_block(nn.Module):
     #downsampling block of the discriminator
@@ -36,7 +36,7 @@ class down_block(nn.Module):
         self.act = act
         self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride,padding=1)
         self.bn = nn.BatchNorm2d(out_channels)
-        self.relu = nn.LeakyReLU(inplace=True, negative_slope=0.1)
+        self.relu = nn.ReLU(inplace=True)
     def forward(self, x):
         x = self.conv(x)
         x = self.bn(x)
@@ -54,21 +54,21 @@ class Generator(nn.Module):
     output: image of shape (bs, out_channels, 64, 64)
     by default the noise is sampled at (bs, 32, 4,4) and outputs an RGB image (3, 64, 64)
     '''
-    def __init__(self, in_channels=32, out_channels=3, up_sizes= [4,2,2,4]):
+    def __init__(self, in_channels=256, out_channels=3, up_sizes= [4,2,2,4]):
         super().__init__()
-        self.fc = FF(1024, 512)
-        self.conv1 = up_block(in_channels, 64)
-        self.conv2 = up_block( 64, 64, kernel_size = 4, stride = up_sizes[0])
-        self.conv3 = up_block(64, 64)
-        self.conv4 = up_block(64, 64, kernel_size = 2, stride=up_sizes[1])
-        self.conv5 = up_block(64, 64)
-        self.conv6 = up_block(64, 32, kernel_size = 2, stride=up_sizes[2])
-        self.conv7 = up_block(32,3,act='sigmoid')
+        self.fc = FF(1024, 4096)
+        self.conv1 = up_block(256, 256)
+        self.conv2 = up_block( 256, 256, kernel_size = 4, stride = up_sizes[0])
+        self.conv3 = up_block(256, 256)
+        self.conv4 = up_block(256, 256, kernel_size = 2, stride=up_sizes[1])
+        self.conv5 = up_block(256, 128)
+        self.conv6 = up_block(128, 64, kernel_size = 2, stride=up_sizes[2])
+        self.conv7 = up_block(64,3,act='tanh')
                               
             
     def forward(self, x):
         x = self.fc(x)
-        x = x.view(-1, 32, 4, 4)
+        x = x.view(-1, 256, 4, 4)
         x = self.conv1(x)
         x = self.conv2(x)
         x = self.conv3(x)
@@ -79,7 +79,6 @@ class Generator(nn.Module):
         return x 
     
 
-
 class Discriminator(nn.Module):
     '''
     Discriminator block
@@ -89,9 +88,9 @@ class Discriminator(nn.Module):
     '''
     def __init__(self, in_channels=3):
         super().__init__()
-        self.conv1 = down_block(in_channels,16)
-        self.conv2 =  down_block(16,32)
-        self.conv3 = down_block(32,64)
+        self.conv1 = down_block(in_channels,256)
+        self.conv2 =  down_block(256,256)
+        self.conv3 = down_block(256,64)
         self.conv4 = down_block(64,1)
         self.flatten = nn.Flatten()
         self.fc = nn.Linear(16, 1)    
@@ -110,15 +109,15 @@ LSGAN losses from https://arxiv.org/pdf/1611.04076.pdf
 
 
 def gen_loss(fake_outs):
-    loss = torch.mean(0.5 * (fake_outs - 1) ** 2)
+    loss = 0.5 * torch.mean((fake_outs - 1) ** 2)
 
     return loss
 
 
 def disc_loss(real_outs, fake_outs):
-    d_loss = 0.5 * (real_outs - 1) ** 2
-    g_loss = 0.5 * fake_outs ** 2
+    d_loss = 0.5 * torch.mean((real_outs-1)**2)
+    g_loss = 0.5 * torch.mean(fake_outs ** 2)
 
-    return torch.mean(d_loss + g_loss)
+    return d_loss + g_loss
 
 
