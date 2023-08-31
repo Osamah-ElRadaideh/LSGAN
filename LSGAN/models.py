3e73a7a2353f2862117f650b8a041bfee45c3661
Osamah-ElRadaideh <142032834+Osamah-ElRadaideh@users.noreply.github.com>
2023-08-31 14:26:48 +0200

    updated model and training loop, images added

--- LSGAN/models.py
+++ LSGAN/models.py
@@ -1,42 +1,50 @@
 import torch.nn as nn
 import torch
+import numpy as np
+import torch.nn.functional as F
+from torch import Tensor
+from torch import autograd
 
 class FF(nn.Module):
     def __init__(self, in_dim, out_dim,):
         super().__init__()
         self.fc = nn.Linear(in_dim, out_dim)
         self.bn = nn.BatchNorm1d(out_dim)
+        self.relu = nn.LeakyReLU(inplace=True, negative_slope=0.2)
     def forward(self, x):
-        return self.bn(self.fc(x))
-
+        return self.relu(self.fc(x))
 class up_block(nn.Module):
     #up_sampling block of the generator
     def __init__(self, in_channels, out_channels, kernel_size=1, stride=1,act='relu'):
         super().__init__()
-        assert act.lower() in ['relu', 'tanh'], f'act must be either relu or tanh, got {act} instead'
+        assert act.lower() in ['relu', 'sigmoid'], f'act must be either relu or sigmoid, got {act} instead'
         self.act = act
         self.conv = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride)
         self.bn = nn.BatchNorm2d(out_channels)
-        self.relu = nn.ReLU(inplace=True)
-        self.tanh = nn.Tanh()
+        self.relu = nn.LeakyReLU(inplace=True, negative_slope=0.2)
+        self.sigmoid = nn.Sigmoid()
     def forward(self, x):
         x = self.conv(x)
-        x = self.bn(x)
+        # x = self.bn(x)
         if self.act =='relu':
             return(self.relu(x))
         else:
             # return x 
-            return(self.tanh(x))
+            return(self.sigmoid(x))
 
 class down_block(nn.Module):
     #downsampling block of the discriminator
-    def __init__(self, in_channels, out_channels, kernel_size=4, stride=2,act='relu'):
+    def __init__(self, in_channels, out_channels, kernel_size=5, stride=2,act='relu'):
         super().__init__()
-        assert act.lower() == 'relu' or act is None, f'act must be either relu or None, got {act} instead'
+        if kernel_size == 1:
+            padding = 0
+        else: 
+            padding = 1
+        assert act.lower() in ['relu', 'none'], f'act must be either relu or none, got {act} instead'
         self.act = act
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride,padding=1)
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride,padding=padding)
         self.bn = nn.BatchNorm2d(out_channels)
-        self.relu = nn.ReLU(inplace=True)
+        self.relu = nn.LeakyReLU(inplace=True, negative_slope=0.2)
     def forward(self, x):
         x = self.conv(x)
         x = self.bn(x)
@@ -47,6 +55,7 @@ class down_block(nn.Module):
         
 
 
+
 class Generator(nn.Module):
     '''
     LSGAN generator
@@ -54,30 +63,27 @@ class Generator(nn.Module):
     output: image of shape (bs, out_channels, 64, 64)
     by default the noise is sampled at (bs, 32, 4,4) and outputs an RGB image (3, 64, 64)
     '''
-    def __init__(self, in_channels=256, out_channels=3, up_sizes= [4,2,2,4]):
+    def __init__(self, latent_size, channel_size = 256, up_sizes= [4,2,2,4]):
         super().__init__()
-        self.fc = FF(1024, 4096)
-        self.conv1 = up_block(256, 256)
-        self.conv2 = up_block( 256, 256, kernel_size = 4, stride = up_sizes[0])
-        self.conv3 = up_block(256, 256)
-        self.conv4 = up_block(256, 256, kernel_size = 2, stride=up_sizes[1])
-        self.conv5 = up_block(256, 128)
-        self.conv6 = up_block(128, 64, kernel_size = 2, stride=up_sizes[2])
-        self.conv7 = up_block(64,3,act='tanh')
+        self.channel_size = channel_size
+        self.ff = FF(latent_size, 4096)
+        self.conv1 = up_block(channel_size, channel_size // 2, kernel_size = 4, stride = up_sizes[0])
+        self.conv2 = up_block(channel_size // 2, channel_size // 4, kernel_size = 2, stride=up_sizes[1])
+        self.conv3 = up_block(channel_size // 4, channel_size // 8, kernel_size = 2, stride=up_sizes[2])
+        self.conv4 = up_block(channel_size // 8 ,3,act='sigmoid')
                               
             
     def forward(self, x):
-        x = self.fc(x)
-        x = x.view(-1, 256, 4, 4)
+        x = self.ff(x)
+        x = x.view(x.shape[0], self.channel_size, 4, 4)
         x = self.conv1(x)
         x = self.conv2(x)
         x = self.conv3(x)
         x = self.conv4(x)
-        x = self.conv5(x)
-        x = self.conv6(x)
-        x = self.conv7(x)
         return x 
-    
+
+
+
 
 class Discriminator(nn.Module):
     '''
@@ -86,26 +92,29 @@ class Discriminator(nn.Module):
     output: probabilities of shape (bs, 1)
     
     '''
-    def __init__(self, in_channels=3):
+    def __init__(self, channel_size=64, in_channels=3):
         super().__init__()
-        self.conv1 = down_block(in_channels,256)
-        self.conv2 =  down_block(256,256)
-        self.conv3 = down_block(256,64)
-        self.conv4 = down_block(64,1)
+        self.conv1 = down_block(in_channels,channel_size)
+        self.conv2 = down_block(channel_size,2  * channel_size)
+        self.conv3 =  down_block(2 * channel_size,4 * channel_size)
+        self.conv4 = down_block(4 * channel_size, 8 * channel_size)
+
         self.flatten = nn.Flatten()
-        self.fc = nn.Linear(16, 1)    
+        self.fc = nn.Linear(4608, 1)    
     def forward(self, x):
+        features = []
         x = self.conv1(x)
         x = self.conv2(x)
         x = self.conv3(x)
         x = self.conv4(x)
-        x = self.fc(self.flatten(x))
-        return x
+        x = self.flatten(x)
+        return self.fc(x), features
 
 
 '''
 LSGAN losses from https://arxiv.org/pdf/1611.04076.pdf
 '''
+
 
 
 def gen_loss(fake_outs):
@@ -115,9 +124,63 @@ def gen_loss(fake_outs):
 
 
 def disc_loss(real_outs, fake_outs):
-    d_loss = 0.5 * torch.mean((real_outs-1)**2)
+    d_loss = 0.5 * torch.mean((real_outs - 1)**2)
     g_loss = 0.5 * torch.mean(fake_outs ** 2)
-
+    # d_loss = torch.mean((1-real_outs)**2)
+    # g_loss = torch.mean(fake_outs**2)
     return d_loss + g_loss
 
+def logistic_g_loss(d_generated_outputs: Tensor) -> Tensor:
+    """
+    Logistic generator loss.
+    Assumes input is D(G(x)), or in our case, D(W(z)).
+    `disc_outputs` of shape (bs,)
+    """
+    # d_generated_outputs = torch.sigmoid(d_generated_outputs)
+    # loss = torch.log(1 - d_generated_outputs).mean()
+    loss = F.softplus(-d_generated_outputs).mean()
+    return loss
 
+def logistic_d_loss(d_real_outputs, d_generated_outputs):
+    """
+    Logistic discriminator loss.
+    `d_real_outputs` (bs,): D(x), or in our case D(c)
+    `d_generated_outputs` (bs,): D(G(x)), or in our case D(W(z))
+    D attempts to push real samples as big as possible (as close to 1.0 as possible), 
+    and push fake ones to 0.0
+    """
+    # d_real_outputs = torch.sigmoid(d_real_outputs)
+    # d_generated_outputs = torch.sigmoid(d_generated_outputs)
+    # loss = -( torch.log(d_real_outputs) + torch.log(1-d_generated_outputs) )
+    # loss = loss.mean()
+    term1 = F.softplus(d_generated_outputs) 
+    term2 = F.softplus(-d_real_outputs)
+    return (term1 + term2).mean()
+
+
+def compute_gp(netD, real_data, fake_data):
+        batch_size = real_data.size(0)
+        # Sample Epsilon from uniform distribution
+        eps = torch.rand(batch_size, 1, 1, 1).to(real_data.device)
+        eps = eps.expand_as(real_data)
+        
+        # Interpolation between real data and fake data.
+        interpolation = eps * real_data + (1 - eps) * fake_data
+        
+        # get logits for interpolated images
+        interp_logits = netD(interpolation)
+        grad_outputs = torch.ones_like(interp_logits)
+        
+        # Compute Gradients
+        gradients = autograd.grad(
+            outputs=interp_logits,
+            inputs=interpolation,
+            grad_outputs=grad_outputs,
+            create_graph=True,
+            retain_graph=True,
+        )[0]
+        
+        # Compute and return Gradient Norm
+        gradients = gradients.view(batch_size, -1)
+        grad_norm = gradients.norm(2, 1)
+        return torch.mean((grad_norm - 1) ** 2) * 10
--- LSGAN/train.py
+++ LSGAN/train.py
@@ -1,33 +1,48 @@
 import numpy as np
 import torch
 import torch.nn as nn
-from models import Generator, Discriminator, gen_loss, disc_loss
+from models import Generator, Discriminator, gen_loss, disc_loss, compute_gp
 from tqdm import tqdm
 from torch.utils.data import DataLoader
 import cv2
 from utils import AHE, collate
+import torch.nn.functional as F
 import lazy_dataset
+from sacred import Experiment
 from torch.utils.tensorboard import SummaryWriter
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-print(device)
+print(f'device set to : {device}')
+
+ex = Experiment('LSGAN', save_git_info=False)
 sw = SummaryWriter()
+
+@ex.config
+def defaults():
+    batch_size = 16
+    g_lr = 0.0001
+    d_lr = 0.00005
+    steps_per_eval = 1000
+    max_steps = 150_000
+    latent_size = 256
 #load the files
 def load_img(example):
     img = cv2.imread(example['image_path'])
-    example['image'] = img.astype(np.float32)
+    example['image'] = img.astype(np.float32) / 255.0
     return example
 
-def prepare_dataset(dataset,batch_size=16):
+@ex.capture
+def prepare_dataset(dataset,batch_size):
     if isinstance(dataset,list):
         dataset = lazy_dataset.new(dataset)
     dataset = dataset.map(load_img)
     dataset = dataset.shuffle()
-    dataset = dataset.batch(batch_size=batch_size)
+    dataset = dataset.batch(batch_size=batch_size, drop_last=True)
     dataset = dataset.map(collate)
     return dataset
 
 path = 'ckpt_latest.pth'
-def main():
+@ex.automain
+def main(batch_size,d_lr,g_lr, steps_per_eval, latent_size,max_steps):
     #model hyperparamters
     #per the LSGAN paper, beta1 os set to 0.5
 
@@ -35,80 +50,90 @@ def main():
     t_ds = db.get_dataset('training_set')
     v_ds = db.get_dataset('validation_set')
     steps = 0
-    gen = Generator().to(device)
+    flag = 'D'
+    counter = 0
+    gen = Generator(latent_size=latent_size).to(device)
     disc = Discriminator().to(device)
     gen.train()
     disc.train()
-    g_optim = torch.optim.Adam(gen.parameters(),lr=1e-4,betas=(0.5,0.999))
-    d_optim = torch.optim.Adam(disc.parameters(),lr=1e-4, betas=(0.5,0.999))
+    g_optim = torch.optim.Adam(gen.parameters(),lr=g_lr)
+    d_optim = torch.optim.Adam(disc.parameters(),lr=d_lr)
 
-    min_val = 10e7
-    running_g_loss = 0
-    running_d_loss = 0 
-    for epoch in range(250):
-        train_ds = prepare_dataset(t_ds)
-        valid_ds = prepare_dataset(v_ds,batch_size=1)
+    for epoch in range(10000):
+        epoch_g_loss = 0
+        epoch_d_loss = 0
+        train_ds = prepare_dataset(t_ds, batch_size=batch_size)
+        valid_ds = prepare_dataset(v_ds, batch_size=1)
         for index,batch in enumerate(tqdm(train_ds)):
             g_optim.zero_grad()
             d_optim.zero_grad()
             images = batch['image']
             images = torch.tensor(np.array(images)).to(device).permute(0,3,1,2) # cv2 loads images as (h,w,3), models take(3,h,w)
-            noise = torch.randn(images.shape[0], 1024).to(device)
-            fakes = gen(noise)
+            noise = torch.randn(images.shape[0], latent_size).to(device)
             p_images = images #for visualisation
-            d_fake = disc(fakes)
+            fakes = gen(noise)
 
             #******************* 
             # generator step
         
             #*******************
+            fakeouts, g_fm = disc(fakes)
+            _, d_fm = disc(images)
+            fm_loss = 0
+            for g,d in zip(g_fm,d_fm):
 
-            loss_g = gen_loss(d_fake)
+                fm_loss += torch.mean(torch.abs(g - d))
+            loss_g =  fm_loss * 10 + gen_loss(fakeouts)
             loss_g.backward()
             g_optim.step()
-            running_g_loss += loss_g.item()
+            epoch_g_loss += loss_g.item()
+               
+
             #*********************  
             
             # discriminator step
 
             #*********************
-            d_real = disc(images)
-            d_fake = disc(fakes.detach())
-            loss_d = disc_loss(d_real, d_fake)
+            d_fake ,_ = disc(fakes.detach())
+            d_real,_ = disc(images)
+            loss_d = disc_loss(d_real, d_fake) 
             loss_d.backward()
             d_optim.step()
-            running_d_loss += loss_d.item()
+            epoch_d_loss += loss_d.item()
+   
+
+
+            
+        
            
-            if steps % 1000 == 0:
+            if steps % steps_per_eval == 0:
                 gen.eval()
                 disc.eval()
                 with torch.no_grad():
-                    for batch in tqdm(valid_ds):
+                    for batch in tqdm(valid_ds[0:1]):
                         images = batch['image']
                         images = torch.tensor(np.array(images)).to(device).permute(0,3,1,2)
-                        noise = torch.randn(images.shape[0], 1024).to(device)
+                        noise = torch.randn(images.shape[0], latent_size).to(device)
 
                         fakes = gen(noise)
-                        d_fake = disc(fakes.detach())
-                        d_real = disc(images)
+                        d_fake, _ = disc(fakes.detach())
+                        d_real, _ = disc(images)
                         g_loss = gen_loss(d_fake)
-                        d_loss = disc_loss(d_real, d_fake)
+                        d_loss  = disc_loss(d_real, d_fake)
 
-                print(f'training losses after {steps} batches  {running_g_loss/ (steps + 1)} |--| {running_d_loss/ (steps + 1)}  ')
-                sw.add_scalar("training/generator_loss",running_g_loss/(steps + 1),steps)
-                sw.add_scalar("training/discriminator_loss",running_d_loss/(steps + 1),steps)
+                
 
 
-                print(f'validation loss after {steps} batches: {g_loss.item()} |---| {d_loss.item()}')
-                sw.add_scalar("validation/generator_loss",g_loss,steps)
-                sw.add_scalar("validation/fake_image_prediction",d_fake,steps)
-                sw.add_scalar("validation/real_image_prediction",d_real,steps)
+                    print(f'validation loss after {steps} batches: {g_loss.item()} |---| {d_loss.item()}')
+                    sw.add_scalar("validation/generator_loss",g_loss,steps)
+                    sw.add_scalar("validation/fake_image_prediction",d_fake,steps)
+                    sw.add_scalar("validation/real_image_prediction",d_real,steps)
 
-                sw.add_scalar("validation/discriminator_loss",d_loss,steps)
-                z = torch.randn(16,1024).to(device)
-                outs = gen(z)
-                sw.add_images("validation/generated_images", outs,steps)
-                sw.add_images("validation/real_images", torch.div(p_images,255.0),steps)
+                    sw.add_scalar("validation/discriminator_loss",d_loss,steps)
+                    z = torch.randn(16,latent_size).to(device)
+                    outs = gen(z)
+                    sw.add_images("validation/generated_images", outs,steps)
+                    sw.add_images("validation/real_images", p_images,steps)
 
 
                 
@@ -125,8 +150,8 @@ def main():
             steps +=1
             gen.train()
             disc.train()
-  
-
-if __name__== '__main__':
-    main()
-
+        sw.add_scalar("training/generator_loss",epoch_g_loss/len(train_ds),epoch)
+        sw.add_scalar("training/discriminator_loss",epoch_d_loss/len(train_ds),epoch)
+        if steps ==   max_steps:
+            print('maximum steps reached....stopping training loop')
+            break
